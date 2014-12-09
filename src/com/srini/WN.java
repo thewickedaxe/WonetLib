27969f9d7fa601141faa70d80913fef4b076f090
Srinivas Suresh Kumar <sudoshmoo@gmail.com>
2014-12-09 09:26:55 +0530

    Update WN.java

--- src/com/srini/WN.java
+++ src/com/srini/WN.java
@@ -12,13 +12,8 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Example program to list links from a URL.
- */
 public class WN 
 {
-	
-	
 	public ArrayList<Definition> getter(String word) throws IOException
 	{
 		ArrayList<Definition> data=new ArrayList<Definition>();
@@ -52,7 +47,6 @@ public class WN
 			    	if(j==0)
 			        {
 			    		d.synonyms=matcher1.group(1).toString();
-			    		//System.out.println(matcher1.group(1));
 			        }
 			    	j++;
 			    }
@@ -73,20 +67,4 @@ public class WN
 		
 		return data;
 	}
-	public static void main(String[] args) throws IOException 
-	{
-       	//url contains the predefined wordnet search url string without the search word itself
-    //	String url="http://wordnetweb.princeton.edu/perl/webwn?s=";
-    ArrayList<Definition> stuff;
-	WN w=new WN();   
-    stuff=w.getter("car");
-    for(Definition d:stuff)
-    {
-    	System.out.println("car");
-    	System.out.println("part of speech: "+d.type);
-    	System.out.println("definition: "+d.meaning);
-    	System.out.println("synonyms: "+d.synonyms);
-    	System.out.println("");
-    }
-    }
 }
